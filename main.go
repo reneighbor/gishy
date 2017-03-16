9d1ef047387f92ef9842392c4b8cfd152e3715a7
Renee Chu <renee.achu@gmail.com>
2017-03-15 17:58:44 -0700

    print out positional args

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("I am here!")
+	searchCriteria := strings.Join(os.Args[1:], " ")
+	fmt.Println("You searched for " + searchCriteria)
 }
+
+
+// 1) We learned that a go program's executable part must be part of "package 
+//main". Saying "go run [directory]" will  compile package main and run the 
+//compiled file 
+
+// 2) We learned that to make the CLI available as a command (ie "$> goshy"), 
+// need to build build the source and output it to a file the same name as
+// the desired command. So "go build -o goshy"
+
+// to make the command available without having to prepend with the path, add
+// the current dir to the path; "export PATH=$PATH:$PWD"
+
+// 3) We learned that we can access positional arguments using os.Args, which
+// returns an array
+
+// 4) We learned that you have to use Google Custom Search to search for images. 
+// We need to create a developer account and generate an API key.
