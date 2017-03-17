6cb65ef8db66334440cf7e678d15634d860551c8
Natalie J. Bennett <nbennett@pivotal.io>
2017-03-16 18:06:31 -0700

    printing urls; notes from futzing around command line
    Signed-off-by: Renee Chu <rchu@pivotal.io>

--- main.go
+++ main.go
@@ -6,9 +6,15 @@ import (
 	"strings"
 )
 
+const GIPHY_URL = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=1"
+
 func main() {
-	searchCriteria := strings.Join(os.Args[1:], " ")
-	fmt.Println("You searched for " + searchCriteria)
+	searchCriteria := strings.Join(os.Args[1:], "+")
+	//fmt.Println("You searched for " + searchCriteria)
+
+	url := GIPHY_URL + "&q=" + searchCriteria
+
+	fmt.Println(url)
 }
 
 
@@ -28,3 +34,14 @@ func main() {
 
 // 4) We learned that you have to use Google Custom Search to search for images. 
 // We need to create a developer account and generate an API key.
+
+// --- 3/16
+
+// 1) We explored giphy as an alternative to Google and it's better (free public API). We can
+// enter a search query in the URL and get a JSON blob with the gif URL
+
+// 2) We can print images to the terminal in iterm using printf
+// https://www.iterm2.com/documentation-images.html
+
+// We learned that we can pipe an images contents into the shell command 'base64' and convert the image
+// to base64 (which we can hopefully print) cat ~/Downloads/cat.gif | base64
